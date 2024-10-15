e2b88798f2a4dda92d677a6e7c52bbef550569e0
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2024-10-15 15:22:45 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
 
+import turtle as t
+import random as subbu
+def Haring_Figure():
+    t.up()
+    t.pensize(4)
+    t.goto(50,200)
+    t.pencolor("black")
+    t.pendown()
+    t.circle(30)
+    t.penup()
+    t.goto(50,190)
+    t.left(45)
+    t.pendown()
+    t.forward(60)
+    t.right(90)
+    t.circle(10,270)
+
+
+Haring_Figure()
